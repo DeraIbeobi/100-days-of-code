8410748a06fffc78d177146b86b2adddc1300d74
DeraIbeobi <40524298+DeraIbeobi@users.noreply.github.com>
2020-10-31 19:55:03 +0800

    Add files via upload
    Day 1: 100 days of code

---
+++ ex1.py
@@ -0,0 +1,55 @@
+#1
+print("*************")
+print("*************")
+print("*************")
+print("*************")
+
+print('')
+
+#2
+print('* * * * * * * *')
+print('*             *')
+print('*             *')
+print('* * * * * * * *')
+
+print('')
+
+#3
+print('*')
+print('**')
+print('***')
+
+print('')
+
+#4
+a = 512-282
+b = 47.48+5
+print(a/b)
+
+#5
+Q = eval(input('Enter a number: '))
+print('The square of',Q,'is', Q*Q)
+
+#6
+x = eval(input('Enter a number: '))
+print(x,2*x,3*x,4*x,5*x, sep='---')
+
+#7
+kg = eval(input('Enter weight in kilogram: '))
+print('weight in pounds:',kg*2.2)
+
+#8
+n1 = eval(input('first number: '))
+n2 = eval(input('second number: '))
+n3 = eval(input('third number: '))
+total = n1+n2+n3
+average = (n1+n2+n3)/3
+print('The total of your numbers are:',total)
+print('The average of your numbers are:',average)
+
+#9
+meal = eval(input('Price of meal: '))
+tpc = eval(input('tip percentage: '))
+tipamount = (tpc/100)*meal
+totalbill = meal+tipamount
+
